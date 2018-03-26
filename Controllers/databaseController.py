8e971ae5f7b7d4dfefd259205af1f05a7016796f
MarkisKelly22 <20728284+markiskelly22@users.noreply.github.com>
2018-03-26 00:11:26 -0400

    Initial Submit of controller

---
+++ Controllers/databaseController.py
@@ -0,0 +1,13 @@
+import pymongo
+
+# Create connection to MLab Database
+uriLocal = "mongodb://127.0.0.1:27017"
+uriLive = "mongodb://<SuperUser>:<Password>@ds123499.mlab.com:23499/draw_and_guess"
+# uriLive = ""
+client = pymongo.MongoClient(uriLocal)
+database = client['draw-and-guess']
+
+# Create retrieval method of JSON objects
+
+# Create post method of JSON object
+
