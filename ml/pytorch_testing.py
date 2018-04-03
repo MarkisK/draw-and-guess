8f2dad2e1dfb0d9e2ee992a8522be57ec518d689
David-Mc <davidbfhemail@gmail.com>
2018-04-03 15:01:32 -0400

    Documentation on neural network added

--- ml/pytorch_testing.py
+++ ml/pytorch_testing.py
@@ -63,37 +63,79 @@
 # Image recognition example using pyTorch
 # Code following: http://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
 
+# ======================================================================
+#     Neural Network for draw-and-guess project
+#
+# In the comments I will explain how the neural network is setup using pyTorch.
+# Note: This is how I (David-Mc) understand it. Take everything I
+# say with a grain of salt. If someone more smarter than me
+# tells you otherwise, go with that.
+#
+# --Referencing--
+# I will use references like so [#]
+# References are related or explain in detail some part of the code
+# The # is a number denoting the reference
+# Find references at the end of the file
+# ======================================================================
+
 import torch
-from torch.autograd import Variable
-import torchvision
-import torchvision.transforms as transforms
-import torch.utils.data as data
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torch.utils.data as data
+import torchvision.transforms as transforms
+from torch.autograd import Variable
 from torchvision.datasets import ImageFolder
 
 
 class Net(nn.Module):
+    # This is the neural network.
+    # nn is the pyTorch neural network module.
+    # nn.Module is a neural network.
+    # we override functions to build the network
     def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 5)
-        self.pool = nn.MaxPool2d(4, 4)
-        self.conv2 = nn.Conv2d(64, 128, 5)
-        self.fc1 = nn.Linear(128 * 12 * 12, 120)
+        self.total_classes = 10
+        # The init function is where we setup the different layers
+        # of the neural network.
+        # We define them in the order we use them, by convention
+
+        # As far as I understand it, there isn't any rhyme or reason to
+        # the in/out values or kernel sizes and resulting accuracy.
+        # You play around with them to see what works for your data.
+        # For each layer, the out and in values need to match
+        # For example, conv1 has out=64, conv2 has in=64
+        super(Net, self).__init__()  # Init the underlying neural network (nn.Module)
+        self.conv1 = nn.Conv2d(3, 64, 5)  # 2D convolution layer(in, out, kernel) [1]
+        self.pool = nn.MaxPool2d(4, 4)  # Max pooling layer(kernel_size, stride) [2]
+        self.conv2 = nn.Conv2d(64, 128, 5)  # Another 2D convolution layer (in, out, kernel) [1]
+        self.fc1 = nn.Linear(128 * 12 * 12, 120)  # Linear transform (in, out) [4]
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc3 = nn.Linear(84, self.total_classes)  # Last layer need output neuron = to total_classes
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 128 * 12 * 12)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        x = self.pool(F.relu(self.conv1(x)))  # Conv Layer1 -> ReLU (activation) -> Max Pool -> x
+        x = self.pool(F.relu(self.conv2(x)))  # Conv Layer2 -> ReLU (activation) -> Max Pool -> x
+        # The following line transforms the dimension to be
+        # 128 * 12 * 12 columns and however many rows fit
+        # that many columns. -1 will end up being the batch size
+        # I figured out 128 * 12 * 12 by looking at x in debug right before
+        # it was transformed by the below line.
+        # x.shape was 20, 128, 12, 12
+        # 20 is the batch size (1st dimension)
+        # Doubt that's how you're supposed to get the number
+        # but, it still worked!
+        x = x.view(-1, 128 * 12 * 12)  # view transform to [batch_size, 18432]
+        x = F.relu(self.fc1(x))  # Linear layer -> ReLU (activation) -> x
+        x = F.relu(self.fc2(x))  # Linear layer -> ReLU (activation) -> x
+        x = self.fc3(x)  # Linear layer -> x
+        return x  # x.shape = [batch_size, total_classes]
 
 
+# This is the transform applied to all images
+# The Normalize portion comes after it's turned into a tensor
+# I'm using the defaults from ResNet for the transform
+# The transforms can be anything, as long as the image
+# is normalized and all images as the same size
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
@@ -101,52 +143,52 @@ transform = transforms.Compose([
                          std=[0.229, 0.224, 0.225]),
 ])
 
+# Create the data loader (training images) and the test loader (test iamges)
 dataset = ImageFolder(root='./images/train', transform=transform)
 dataloader = data.DataLoader(dataset, batch_size=20,
                              shuffle=True, num_workers=2)
 testset = ImageFolder(root='./images/test', transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=20,
                                          shuffle=False, num_workers=2)
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-net = Net()
+net = Net()  # Create instance of our neural network, untrained
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 print('beginning training...')
 
+# epoch is just the number of times we want to
+# train using all the training images
 for epoch in range(5):
     print('epoch ' + str(epoch))
-    running_loss = 0.0
-    for i, data in enumerate(dataloader, 0):
+    running_loss = 0.0  # Keeps track of how badly the network is classifying
+    for i, data in enumerate(dataloader, 0):  # Iterate through the mini-batches
         if i % 200 == 0:
             print('dataloader: ' + str(i))
         # get the inputs
-        inputs, labels = data
+        inputs, labels = data  # inputs = mini-batch images, labels = mini-batch labels
 
         # wrap them in Variable
         # uncomment following and comment one after to enable GPU processing
         # inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
         inputs, labels = Variable(inputs), Variable(labels)
+        # Variable's are used to allow for automatic back propagation (shown later) [5]
 
         # zero the parameter gradients
         optimizer.zero_grad()
 
         # forward + backward + optimize
-        outputs = net(inputs)
-        # labels = torch.unsqueeze(torch.ones([]), 0)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+        outputs = net(inputs)  # get the guesses for all the images in the mini-batch
+        loss = criterion(outputs, labels)  # Get loss function (compare guesses with correct labels)
+        loss.backward()  # autograd automatic back-propagation [5]
+        optimizer.step()  # optimization step
 
-        # print statistics
-        running_loss += loss.data[0]
-        if i % 2000 == 1999:  # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
-        print('finished {}. running_loss: {}'.format(i, running_loss))
+        # # print statistics -- optional
+        # running_loss += loss.data[0]
+        # if i % 2000 == 1999:  # print every 2000 mini-batches
+        #     print('[%d, %5d] loss: %.3f' %
+        #           (epoch + 1, i + 1, running_loss / 2000))
+        #     running_loss = 0.0
+        # print('finished {}. running_loss: {}'.format(i, running_loss))
 
 print('Finished Training')
 
@@ -163,38 +205,9 @@ for data in testloader:
 print('Accuracy of the network on the 10000 test images: %d %%' % (
     100 * correct / total))
 
-# Example output of a session with five epochs
-# Files already downloaded and verified
-# Files already downloaded and verified
-# [1,  2000] loss: 2.206
-# [1,  4000] loss: 1.850
-# [1,  6000] loss: 1.652
-# [1,  8000] loss: 1.551
-# [1, 10000] loss: 1.500
-# [1, 12000] loss: 1.477
-# [2,  2000] loss: 1.403
-# [2,  4000] loss: 1.358
-# [2,  6000] loss: 1.332
-# [2,  8000] loss: 1.313
-# [2, 10000] loss: 1.321
-# [2, 12000] loss: 1.265
-# [3,  2000] loss: 1.211
-# [3,  4000] loss: 1.209
-# [3,  6000] loss: 1.201
-# [3,  8000] loss: 1.157
-# [3, 10000] loss: 1.190
-# [3, 12000] loss: 1.174
-# [4,  2000] loss: 1.098
-# [4,  4000] loss: 1.081
-# [4,  6000] loss: 1.097
-# [4,  8000] loss: 1.112
-# [4, 10000] loss: 1.100
-# [4, 12000] loss: 1.076
-# [5,  2000] loss: 1.007
-# [5,  4000] loss: 1.002
-# [5,  6000] loss: 1.017
-# [5,  8000] loss: 1.034
-# [5, 10000] loss: 1.027
-# [5, 12000] loss: 1.038
-# Finished Training
-# Accuracy of the network on the 10000 test images: 61 %
+# ___References___
+# [1] Convolutional Neural Networks (CNN): https://hooktube.com/watch?v=FTr3n7uBIuE
+# [2] Max pooling explained w/CNN details: https://cs231n.github.io/convolutional-networks/
+# [3] View function: https://stackoverflow.com/questions/42479902/how-view-method-works-for-tensor-in-torch/42482819#42482819
+# [4] Linear transform layers: http://pytorch.org/docs/master/_modules/torch/nn/modules/linear.html
+# [5] Variables and autograd: http://pytorch.org/tutorials/beginner/examples_autograd/two_layer_net_autograd.html
