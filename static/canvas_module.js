67d6efd50b770c2c26e1e583129d9d900cda8c43
Marcus <madwards28@gmail.com>
2018-04-04 13:26:59 -0400

    changed default stroke width

--- static/canvas_module.js
+++ static/canvas_module.js
@@ -1,7 +1,7 @@
 var lc = LC.init(document.getElementById("lc"), {
     imageURLPrefix: '../static/_assets/lc-images',
     toolbarPosition: 'bottom',
-    defaultStrokeWidth: 2,
+    defaultStrokeWidth: 3,
     strokeWidths: [1, 2, 3, 5, 30]
 });
 
