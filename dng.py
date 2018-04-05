2761873740481c7a1df8978de07a22412037f1db
Marcus <madwards28@gmail.com>
2018-04-05 13:33:23 -0400

    added methods to route POSTs

--- dng.py
+++ dng.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    # if request.method == 'POST':
+    #     code to post
     return render_template('index.html')
 
-
 if __name__ == "__main__":
+    # disable debug mode for production
     app.run(debug=True)
