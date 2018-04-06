55c23639e7892caf35525548bdf22028f39e11df
madwards28 <madwards28@gmail.com>
2018-04-06 13:41:45 -0400

    added about page

--- dng.py
+++ dng.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 
-
 app = Flask(__name__)
 
 
@@ -9,6 +8,12 @@ def index():
     # if request.method == 'POST':
     #     code to post
     return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 if __name__ == "__main__":
     # disable debug mode for production
