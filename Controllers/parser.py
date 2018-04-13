0a3aa67e8bb2cc4c94aca7269d43ef1801f4aac6
Markis <markisrkelly22@gmail.com>
2018-04-13 02:23:09 -0400

    Checked in Parser.py

---
+++ Controllers/parser.py
@@ -0,0 +1,33 @@
+# import json
+from pprint import pprint
+
+
+class DGObjectModel:
+
+    def __init__(self):
+        self.data = {'word': 'test', 'key_id': 1234, 'country_code': 'us',
+                     'timestamp': "", 'recognized': False,
+                     'image': []}
+
+    def parse_json_request(self, json_passed):
+        # TODO write parser for json string object that is passed from front end
+
+        # print(type(json_passed))
+        # pprint(json_passed)
+
+        tup = json_passed.get('shapes')
+
+        pprint(list(json_passed.get('shapes')))
+
+        self.data['word']
+        self.data['key_id']
+        self.data['country_code']
+        self.data['timestamp']
+        self.data['recognized']
+        self.data['image'] = tup
+
+        return self.data
+
+
+
+
