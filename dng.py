8cad47b6b5eeeb9417dfbbeec0dc2a43e44cd548
Marcus Edwards <marcusedwards108@gmail.com>
2018-04-12 23:07:05 -0400

    POST request from canvas

--- dng.py
+++ dng.py
@@ -5,15 +5,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # if request.method == 'POST':
-    #     code to post
-    return render_template('index.html')
+    if request.method == 'POST':
+        lc_image = request.get_json()
 
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
-
 
 if __name__ == "__main__":
     # disable debug mode for production
--- static/canvas_module.js
+++ static/canvas_module.js
@@ -1,5 +1,4 @@
-
-
+// Canvas init
 var lc = LC.init(document.getElementById("lc"), {
     imageURLPrefix: '../static/_assets/lc-images',
     toolbarPosition: 'hidden',
@@ -8,7 +7,31 @@ var lc = LC.init(document.getElementById("lc"), {
     tools: [LC.tools.Pencil, LC.tools.Eraser]
 });
 
-function jsonify() {
-    var obj = JSON.stringify(lc.getSnapshot(['shapes', 'imageSize', 'position', 'scale']))
-    document.getElementById("output").innerHTML = obj;
-}
+
+// POST Drawing to Flask
+$(function () {
+    $('#submit').click(function () {  // Set to run when button with id 'guess' is clicked
+
+        var image_data = JSON.stringify(lc.getSnapshot(['shapes', 'imageSize', 'position', 'scale']));  //gets image from canvas
+
+        $.ajax({
+            url: '/',
+            data: image_data,
+            type: 'POST',
+            contentType: "application/json; charset=utf-8",
+            success: function () {
+                alert('POSTed')
+            },
+            error: function () {
+                alert('error')
+            }
+        });
+    });
+});
+
+
+// uncomment this to debug straight to page (not advised)
+// function postImage() {
+//     var obj = JSON.stringify(lc.getSnapshot(['shapes', 'imageSize', 'position', 'scale']))
+//     document.getElementById("output").innerHTML = obj;
+// }
