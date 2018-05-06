d40eb549c4a494d67863b74d1036e9e1a5317f31
Markis <markisrkelly22@gmail.com>
2018-05-06 08:31:19 -0400

    change package

--- dng.py
+++ dng.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, jsonify
 import base64
 from PIL import Image
 
-from neural_net import make_guess, load_model, Net
+from ml.neural_net import make_guess, load_model, Net
 
 app = Flask(__name__)
 # Create and load pre-trained neural network
